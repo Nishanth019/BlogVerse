dda4ceecf5b999a6397f0406d8074383abe158d7
Nishanth <122528380+Nishanth019@users.noreply.github.com>
2023-10-07 02:07:30 +0530

    Update page.jsx

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -6,7 +6,7 @@ import { Metadata } from 'next';
 
 async function getData(id) {
   console.log(id);
-  const res = await fetch(`http://localhost:3000/api/posts/${id}`, {
+  const res = await fetch(`${process.env.NEXTAUTH_URL}/api/posts/${id}`, {
     cache: 'no-store',
   });
 
